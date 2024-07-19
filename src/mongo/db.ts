f3cfd8f45726b668ba14238873712732a0cf6494
romannjoroge <roman.njoroge@njuguna.com>
2024-07-19 23:25:54 +0300

    feat: stuff

--- src/mongo/db.ts
+++ src/mongo/db.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import { MongoClient } from 'mongodb'
-import { Embedding } from 'openai/resources'
 
 const client = new MongoClient(process.env.MONGO_CONN_STRING!)
 const DATABASE = client.db("convergence-stash")
@@ -23,5 +22,19 @@ export interface IUSERCHATS {
     }[]
 }
 
+export type Body = 'senate' | 'national' | 'executive' | 'individual';
+export type Stage = 'memoranda' | 'second-reading';
+
+export interface IBILLS {
+    name: string,
+    sponsor: string,
+    stage: Stage,
+    body: Body,
+    summary: string,
+    feedbackEmail: string,
+    pdf: BinaryData
+}
+
 export const USERCHATS = DATABASE.collection<IUSERCHATS>("user_chats");
-export const EMBEDDINGS = DATABASE.collection<IEMBEDDINGS>("embeddings")
+export const EMBEDDINGS = DATABASE.collection<IEMBEDDINGS>("embeddings");
+export const BILLS = DATABASE.collection<IBILLS>("bills");
--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,3 +1,4 @@
 export * from "./db";
 export * from './vector-search';
 export * from './user-chat';
+export * from './bills';
